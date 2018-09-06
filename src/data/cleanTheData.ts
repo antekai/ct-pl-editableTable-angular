74114347120b544a204452553ea78bbbc032e16c
Andreas Teneketzoglou <antekai00@gmail.com>
2018-09-06 10:32:28 +0200

    cleaup data preprocess function(cleanTheData.ts)

--- src/data/cleanTheData.ts
+++ src/data/cleanTheData.ts
@@ -1,32 +1,9 @@
-import * as moment from 'moment';
-
-const cleanTheData = data => {
-  const timeOptions = { hour: '2-digit', minute: '2-digit' };
-
-  const clean = data
+const cleanTheData = data =>
+  data
     .map(({ a, campaignid, userid, frienddomainid, ...keepRest }) => keepRest)
-    .map(obj => {
-      obj.time = new Date(obj.date).toLocaleTimeString('en-DE', timeOptions);
-      obj.germanDate = new Date(obj.date).toLocaleDateString('en-DE');
-      // obj.time = moment(obj.date).format('HH:mm');
-      // obj.germanDate = moment(obj.date).format('DD.MM.YYYY');
-      return obj;
-    })
-    // .map(({ date, ...keepRest }) => keepRest)
-    // .map(({ germanDate: date, ...keepRest }) => ({ date, ...keepRest }))
     .map((obj, index) => {
       obj.key = index.toString();
       return obj;
     });
-  // .map(x => {
-  //   x.freeclickString = x.freeclick ? 'true' : 'false';
-  //   return x;
-  // })
-  // .map(({ freeclick, ...keepRest }) => keepRest)
-  // .map(({ freeclickString: freeclick, ...keepRest }) => ({
-  //   freeclick,
-  //   ...keepRest
-  // }));
-  return clean;
-};
+
 export default cleanTheData;
